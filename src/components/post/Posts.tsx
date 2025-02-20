0b95952b7a82c457889eaf29fbff89064ddb1c1d
Zillur2023 <zillurrahmanbd12@gmail.com>
2025-02-20 23:44:57 +0600

    Add PostSkeloton and display PostSkeleton during posts fetch pending state

--- src/components/post/Posts.tsx
+++ src/components/post/Posts.tsx
@@ -46,6 +46,7 @@ import { generatePDF } from "@/utils/generatePDF";
 import useDebounce from "@/hooks/debounce.hooks";
 import Loading from "../UI/Loading";
 import { useUser } from "@/context/user.provider";
+import { PostSkeleton } from "../shared/Skeleton";
 
 interface PostsProps {
   postId?: string;
@@ -145,222 +146,218 @@ const Posts: React.FC<PostsProps> = ({ postId, comment = true }) => {
   };
 
   return (
-    <>
+    <div className="mt-6 space-y-6 max-w-full sm:max-w-[600px] md:max-w-[700px] lg:max-w-[800px] mx-auto">
       {!postData ? (
-        <Loading />
+        <PostSkeleton />
       ) : (
         postData?.data?.length === 0 && <NoDataFound />
       )}
-      <div className="mt-6 space-y-6 max-w-full sm:max-w-[600px] md:max-w-[700px] lg:max-w-[800px] mx-auto">
-        {!postId && postData?.data?.length > 0 && (
-          <div className="flex flex-col sm:flex-row  items-center justify-between ">
-            <input
-              type="text"
-              placeholder="Search..."
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-              className="border rounded p-2 mb-4"
-            />
-            <select
-              value={sortBy}
-              onChange={(e) => setSortBy(e.target.value)}
-              className=" border-medium rounded p-2 mb-4"
-            >
-              <option value="">Sort</option>
-              {sortOptions.map((sort) => (
-                <option key={sort} value={sort}>
-                  {sort}
-                </option>
-              ))}
-            </select>
-            <select
-              value={category}
-              onChange={(e) => setCategory(e.target.value)}
-              className="border-medium rounded p-2 mb-4"
-            >
-              <option value="">Select category</option>
-              {categoryOptions.map((category) => (
-                <option key={category} value={category}>
-                  {category}
-                </option>
-              ))}
-            </select>
-          </div>
-        )}
-
-        {postData?.data?.map((post) => (
-          <Card
-            ref={postRef}
-            key={post._id}
-            isFooterBlurred
-            className=" w-full p-0 md:p-5 "
+      {!postId && postData?.data?.length > 0 && (
+        <div className="flex flex-col sm:flex-row  items-center justify-between ">
+          <input
+            type="text"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+            className="border rounded p-2 mb-4"
+          />
+          <select
+            value={sortBy}
+            onChange={(e) => setSortBy(e.target.value)}
+            className=" border-medium rounded p-2 mb-4"
           >
-            {/* Author Info */}
-            <CardHeader className=" justify-between ">
-              <Author author={post?.author} nameClass="text-lg font-semibold" />
-              <div className="flex items-center gap-3">
-                {post?.author?._id !== userData?.data?._id && (
-                  <CustomButton
-                    onClick={() =>
-                      handleUpdateFollowUnfollow(post?.author?._id)
-                    }
-                    buttonId="followOrUnfollow"
-                    btnClass=" "
-                  >
-                    {userData?.data?.following?.includes(post?.author?._id)
-                      ? "Unfollow"
-                      : "Follow"}
-                  </CustomButton>
-                )}
-                {post?.author?._id === userData?.data?._id && (
-                  <PostUpdate
-                    updatePostData={post}
-                    btn={
-                      <Pencil className="bg-gray-300 p-1 rounded-md w-full h-full" />
-                    }
-                  />
-                )}
+            <option value="">Sort</option>
+            {sortOptions.map((sort) => (
+              <option key={sort} value={sort}>
+                {sort}
+              </option>
+            ))}
+          </select>
+          <select
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}
+            className="border-medium rounded p-2 mb-4"
+          >
+            <option value="">Select category</option>
+            {categoryOptions.map((category) => (
+              <option key={category} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </div>
+      )}
 
-                {post?.author?._id === userData?.data?._id && (
-                  <CustomModal
-                    title=""
-                    openButton={
-                      <Trash2 className="text-red-500 cursor-pointer bg-gray-300 p-1 rounded-md w-full h-full" />
-                    }
-                    actionButtonText="Delete"
-                    onUpdate={() => handleDeleteClick(post?._id)}
-                  >
-                    <p className=" text-red-500 font-semibold text-medium">
-                      {" "}
-                      Are your sure to delete{" "}
-                    </p>
-                  </CustomModal>
-                )}
-              </div>
-            </CardHeader>
-            <CardBody>
-              {post?.isPremium && (
-                <div className=" flex justify-end  ">
-                  {" "}
-                  <span className=" border-1  font-normal text-green-500 py-0 px-2 rounded-md">
-                    Premium
-                  </span>{" "}
-                </div>
-              )}
-              <p className=" py-2 text-lg font-medium">#{post?.category} </p>
-              {/* Post Title */}
-              <p className=" mb-3 text-xl font-medium">{post?.title}</p>
-
-              {/* Post Image */}
-              {post.image && (
-                <div className=" w-full h-[250px] flex justify-center items-center ">
-                  <Image
-                    shadow="sm"
-                    radius="lg"
-                    width="100%"
-                    height={250}
-                    alt={post?.category}
-                    // className=" object-cover "
-                    src={post?.image}
-                  />
-                </div>
-              )}
-
-              {/* Post Content */}
-              <p className="my-5">{post.content}</p>
-            </CardBody>
-
-            <CardFooter className="justify-between ">
-              {/* Post Interactions */}
-              {/* <Tooltip content={post?.upvotes?.[index+1]?.name}> */}
-
-              <CustomButton
-                onClick={() => handleUpvote(post._id)}
-                buttonId="upvote"
-                data={post?.upvotes}
-              >
-                <ThumbsUp
-                  size={18}
-                  className={`${
-                    post?.upvotes?.some(
-                      (item) => item?._id === userData?.data?._id
-                    )
-                      ? "text-blue-600 fill-current"
-                      : "text-gray-600"
-                  }`}
-                />
-
-                <span>{post.upvotes.length}</span>
-              </CustomButton>
-
-              <CustomButton
-                onClick={() => handleDownvote(post._id)}
-                buttonId="downvote"
-                data={post?.downvotes}
-              >
-                <ThumbsDown
-                  size={18}
-                  className={`${
-                    post?.downvotes?.some(
-                      (item) => item?._id === userData?.data?._id
-                    )
-                      ? "text-blue-600 fill-current"
-                      : "text-gray-600"
-                  }`}
-                />
-                <span>{post.downvotes.length}</span>
-              </CustomButton>
-
-              <div className="">
-                <Button
-                  size="sm"
-                  className="flex items-center  bg-transparent hover:bg-gray-300 "
-                  onClick={() => handleCommentClick(post._id)}
+      {postData?.data?.map((post) => (
+        <Card
+          ref={postRef}
+          key={post._id}
+          isFooterBlurred
+          className=" w-full p-0 md:p-5 "
+        >
+          {/* Author Info */}
+          <CardHeader className=" justify-between ">
+            <Author author={post?.author} nameClass="text-lg font-semibold" />
+            <div className="flex items-center gap-3">
+              {post?.author?._id !== userData?.data?._id && (
+                <CustomButton
+                  onClick={() => handleUpdateFollowUnfollow(post?.author?._id)}
+                  buttonId="followOrUnfollow"
+                  btnClass=" "
                 >
-                  <MessageCircle size={18} />
-                  <span>{post?.comments?.length}</span>
-                </Button>
+                  {userData?.data?.following?.includes(post?.author?._id)
+                    ? "Unfollow"
+                    : "Follow"}
+                </CustomButton>
+              )}
+              {post?.author?._id === userData?.data?._id && (
+                <PostUpdate
+                  updatePostData={post}
+                  btn={
+                    <Pencil className="bg-gray-300 p-1 rounded-md w-full h-full" />
+                  }
+                />
+              )}
+
+              {post?.author?._id === userData?.data?._id && (
+                <CustomModal
+                  title=""
+                  openButton={
+                    <Trash2 className="text-red-500 cursor-pointer bg-gray-300 p-1 rounded-md w-full h-full" />
+                  }
+                  actionButtonText="Delete"
+                  onUpdate={() => handleDeleteClick(post?._id)}
+                >
+                  <p className=" text-red-500 font-semibold text-medium">
+                    {" "}
+                    Are your sure to delete{" "}
+                  </p>
+                </CustomModal>
+              )}
+            </div>
+          </CardHeader>
+          <CardBody>
+            {post?.isPremium && (
+              <div className=" flex justify-end  ">
+                {" "}
+                <span className=" border-1  font-normal text-green-500 py-0 px-2 rounded-md">
+                  Premium
+                </span>{" "}
               </div>
+            )}
+            <p className=" py-2 text-lg font-medium">#{post?.category} </p>
+            {/* Post Title */}
+            <p className=" mb-3 text-xl font-medium">{post?.title}</p>
+
+            {/* Post Image */}
+            {post.image && (
+              <div className=" w-full h-[250px] flex justify-center items-center ">
+                <Image
+                  shadow="sm"
+                  radius="lg"
+                  width="100%"
+                  height={250}
+                  alt={post?.category}
+                  // className=" object-cover "
+                  src={post?.image}
+                />
+              </div>
+            )}
+
+            {/* Post Content */}
+            <p className="my-5">{post.content}</p>
+          </CardBody>
+
+          <CardFooter className="justify-between ">
+            {/* Post Interactions */}
+            {/* <Tooltip content={post?.upvotes?.[index+1]?.name}> */}
+
+            <CustomButton
+              onClick={() => handleUpvote(post._id)}
+              buttonId="upvote"
+              data={post?.upvotes}
+            >
+              <ThumbsUp
+                size={18}
+                className={`${
+                  post?.upvotes?.some(
+                    (item) => item?._id === userData?.data?._id
+                  )
+                    ? "text-blue-600 fill-current"
+                    : "text-gray-600"
+                }`}
+              />
+
+              <span>{post.upvotes.length}</span>
+            </CustomButton>
+
+            <CustomButton
+              onClick={() => handleDownvote(post._id)}
+              buttonId="downvote"
+              data={post?.downvotes}
+            >
+              <ThumbsDown
+                size={18}
+                className={`${
+                  post?.downvotes?.some(
+                    (item) => item?._id === userData?.data?._id
+                  )
+                    ? "text-blue-600 fill-current"
+                    : "text-gray-600"
+                }`}
+              />
+              <span>{post.downvotes.length}</span>
+            </CustomButton>
+
+            <div className="">
               <Button
                 size="sm"
                 className="flex items-center  bg-transparent hover:bg-gray-300 "
+                onClick={() => handleCommentClick(post._id)}
               >
-                <Share2 size={18} />
-                {/* <span>{post.comments?.length}</span> */}
+                <MessageCircle size={18} />
+                <span>{post?.comments?.length}</span>
               </Button>
-              {/* <Button
+            </div>
+            <Button
+              size="sm"
+              className="flex items-center  bg-transparent hover:bg-gray-300 "
+            >
+              <Share2 size={18} />
+              {/* <span>{post.comments?.length}</span> */}
+            </Button>
+            {/* <Button
                   size="sm"
                   className="flex items-center  bg-transparent hover:bg-gray-300 "
                 >
                    <Download size={18} onClick={() => generatePDF(post)} className="bg-gray-300 p-1 rounded-md w-full h-full" />
                 </Button> */}
-              <Button
-                size="sm"
-                className="flex items-center  bg-transparent hover:bg-gray-300 "
-                onClick={() => generatePDF(postRef)}
-              >
-                {" "}
-                <Download size={18} />{" "}
-              </Button>
-            </CardFooter>
-            <Comment
-              postId={post?._id}
-              openButton={
-                <p className=" font-semibold my-3 cursor-pointer hover:underline">
-                  {/* See all comments */}
-                  {/* {comment ? "See all comment" : " "} */}
-                  {post?.comments?.length > 2 && comment
-                    ? "See all comment"
-                    : " "}
-                </p>
-              }
-              comment={comment ? true : false}
-              focusRef={(el) => (inputRefs.current[post._id] = el)}
-            />
-          </Card>
-        ))}
-      </div>
-    </>
+            <Button
+              size="sm"
+              className="flex items-center  bg-transparent hover:bg-gray-300 "
+              onClick={() => generatePDF(postRef)}
+            >
+              {" "}
+              <Download size={18} />{" "}
+            </Button>
+          </CardFooter>
+          <Comment
+            postId={post?._id}
+            openButton={
+              <p className=" font-semibold my-3 cursor-pointer hover:underline">
+                {/* See all comments */}
+                {/* {comment ? "See all comment" : " "} */}
+                {post?.comments?.length > 2 && comment
+                  ? "See all comment"
+                  : " "}
+              </p>
+            }
+            comment={comment ? true : false}
+            focusRef={(el) => (inputRefs.current[post._id] = el)}
+          />
+        </Card>
+      ))}
+    </div>
   );
 };
 
