773d6017a36f4c623fab5e5a0690d0f33a2cd404
Zillur2023 <zillurrahmanbd12@gmail.com>
2025-03-03 23:47:55 +0600

    Add routes for friends and marketplace pages

---
+++ src/app/(commonLayout)/marketplace/page.tsx
@@ -0,0 +1,117 @@
+"use client";
+import Sidebar from "@/components/UI/Sidebar";
+import { Card, CardBody, CardFooter, Image } from "@nextui-org/react";
+
+const MarketplacePage = () => {
+  const list = [
+    {
+      title: "Orange",
+      img: "/images/fruit-1.jpeg",
+      price: "$5.50",
+    },
+    {
+      title: "Tangerine",
+      img: "/images/fruit-2.jpeg",
+      price: "$3.00",
+    },
+    {
+      title: "Raspberry",
+      img: "/images/fruit-3.jpeg",
+      price: "$10.00",
+    },
+    {
+      title: "Lemon",
+      img: "/images/fruit-4.jpeg",
+      price: "$5.30",
+    },
+    {
+      title: "Avocado",
+      img: "/images/fruit-5.jpeg",
+      price: "$15.70",
+    },
+    {
+      title: "Lemon 2",
+      img: "/images/fruit-6.jpeg",
+      price: "$8.00",
+    },
+    {
+      title: "Banana",
+      img: "/images/fruit-7.jpeg",
+      price: "$7.50",
+    },
+    {
+      title: "Watermelon",
+      img: "/images/fruit-8.jpeg",
+      price: "$12.20",
+    },
+    {
+      title: "Banana",
+      img: "/images/fruit-7.jpeg",
+      price: "$7.50",
+    },
+    {
+      title: "Watermelon",
+      img: "/images/fruit-8.jpeg",
+      price: "$12.20",
+    },
+    {
+      title: "Banana",
+      img: "/images/fruit-7.jpeg",
+      price: "$7.50",
+    },
+    {
+      title: "Watermelon",
+      img: "/images/fruit-8.jpeg",
+      price: "$12.20",
+    },
+    {
+      title: "Banana",
+      img: "/images/fruit-7.jpeg",
+      price: "$7.50",
+    },
+    {
+      title: "Watermelon",
+      img: "/images/fruit-8.jpeg",
+      price: "$12.20",
+    },
+  ];
+  return (
+    <div className=" flex  justify-center gap-2 my-5  ">
+      <div className="hidden lg:block  lg:w-[20%] sticky top-[65px] h-[calc(100vh-65px)] overflow-y-auto ">
+        <Sidebar />
+      </div>
+
+      {/* <div className={`hidden md:block  md:w-[80%]`}> */}
+      <div className=" lg:block    mx-auto w-full md:w-[80%] ">
+        <div className="gap-2 grid grid-cols-2 sm:grid-cols-4 ">
+          {list.map((item, index) => (
+            /* eslint-disable no-console */
+            <Card
+              key={index}
+              isPressable
+              shadow="sm"
+              onPress={() => console.log("item pressed")}
+            >
+              <CardBody className="overflow-visible p-0">
+                <Image
+                  alt={item.title}
+                  className="w-full object-cover h-[140px]"
+                  radius="lg"
+                  shadow="sm"
+                  src={item.img}
+                  width="100%"
+                />
+              </CardBody>
+              <CardFooter className="text-small justify-between">
+                <b>{item.title}</b>
+                <p className="text-default-500">{item.price}</p>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MarketplacePage;
